01ea121ee53d689d6a8c75f964f2311b8d5f3e22
hyeumm <20221159@sungshin.ac.kr>
2024-04-03 06:37:37 +0900

    [FEAT]회원가입 입력정보 검사

--- app/src/main/java/com/sopt/now/SignUpActivity.kt
+++ app/src/main/java/com/sopt/now/SignUpActivity.kt
@@ -1,20 +1,35 @@
 package com.sopt.now
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.sopt.now.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding:ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    //회원가입 가능한지 검사
+    private fun SignUp(id:String,pw:String,nick:String,etc:String):Boolean {
+        var signUpBool = false
+        val message = when {
+            id.isEmpty() || pw.isEmpty() || nick.isEmpty() || etc.isEmpty() -> "모든 항목을 입력해주세요."
+            id.length !in 6..10 -> "아이디를 다시 설정하세요."
+            pw.length !in 8..12 -> "비밀번호를 다시 설정하세요."
+            nick.isBlank() || nick.length != nick.trim().length -> "닉네임을 다시 설정하세요."
+            etc.length !in 1..Int.MAX_VALUE -> "하고싶은 말을 다시 설정하세요."
+            else -> {
+                signUpBool = true
+                "회원가입에 성공했습니다."
+            }
         }
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        return signUpBool
     }
 }
