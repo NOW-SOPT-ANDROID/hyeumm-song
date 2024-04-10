39cc7715b8552497d4e2015ec4f91f59340b5c0c
hyeumm <20221159@sungshin.ac.kr>
2024-04-11 02:25:19 +0900

    feat/#5: 메뉴와 프래그먼트 연결

--- app/src/main/java/com/sopt/now/MainActivity.kt
+++ app/src/main/java/com/sopt/now/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sopt.now
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.sopt.now.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,12 +12,47 @@ class MainActivity : AppCompatActivity() {
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val currentFragment = supportFragmentManager.findFragmentById(binding.fcvHome.id)
+        if (currentFragment == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fcv_home, HomeFragment())
+                .commit()
+        }
+
+        clickBottomNavigation()
         //로그인화면에서 받아온 id/pw 화면에 출력
-        getUserInfo()
+        //getUserInfo()
     }
-    private fun getUserInfo() {
-        binding.tvMainNick.text = intent.getStringExtra("nick")
-        binding.tvMainId.text = intent.getStringExtra("id")
-        binding.tvMainPw.text = intent.getStringExtra("pw")
+    private fun clickBottomNavigation() {
+        binding.bnvHome.setOnItemSelectedListener{
+            when (it.itemId) {
+                R.id.menu_home-> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+
+                R.id.menu_search-> {
+                    replaceFragment(SearchFragment())
+                    true
+                }
+
+                R.id.menu_mypage-> {
+                    replaceFragment(MyPageFragment())
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcv_home, fragment)
+            .commit()
+    }
+//    private fun getUserInfo() {
+//        binding.tvMainNick.text = intent.getStringExtra("nick")
+//        binding.tvMainId.text = intent.getStringExtra("id")
+//        binding.tvMainPw.text = intent.getStringExtra("pw")
+//    }
 }
