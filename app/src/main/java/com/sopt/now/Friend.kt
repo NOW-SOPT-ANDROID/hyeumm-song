3205e16d46a21268e89d12c35e1832d7396f256a
hyeumm <20221159@sungshin.ac.kr>
2024-04-12 00:42:45 +0900

    del/#5 user 데이터 클래스 삭제

--- app/src/main/java/com/sopt/now/Friend.kt
+++ app/src/main/java/com/sopt/now/Friend.kt
@@ -2,11 +2,6 @@ package com.sopt.now
 
 import androidx.annotation.DrawableRes
 
-data class User(
-    @DrawableRes val profileImage: Int,
-    val name: String,
-    val selfDescription: String
-)
 data class Friend(
     @DrawableRes val profileImage: Int,
     val name: String,
