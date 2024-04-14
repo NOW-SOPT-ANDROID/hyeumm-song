69ccff059ded0e4b7c9ba5bd2021a9c8ac59970e
hyeumm <20221159@sungshin.ac.kr>
2024-04-15 01:06:01 +0900

    mod/#5: 뷰홀더 분리 데이터 분리

---
+++ app/src/main/java/com/sopt/now/MockFriendList.kt
@@ -0,0 +1,66 @@
+package com.sopt.now
+
+object MockFriendList {
+    val mockFriendList = listOf(
+        Friend(
+            profileImage = R.drawable.main,
+            name = "송혜음",
+            selfDescription = "멀티 뷰 리싸이클러뷰!",
+            0
+        ),
+        Friend(
+            profileImage = R.drawable.main,
+            name = "이의경",
+            selfDescription = "다들 빨리 끝내고 뒤풀이 가고 싶지? ㅎㅎ 아직 반도 안왔어 ^&^",
+            1
+        ),
+        Friend(
+            profileImage = R.drawable.main,
+            name = "우상욱",
+            selfDescription = "나보다 안드 잘하는 사람 있으면 나와봐",
+            1
+        ),
+        Friend(
+            profileImage = R.drawable.main,
+            name = "배지현",
+            selfDescription = "표정 풀자 ^^",
+            1
+        ),
+        Friend(
+            profileImage = R.drawable.main,
+            name = "이의경",
+            selfDescription = "다들 빨리 끝내고 뒤풀이 가고 싶지? ㅎㅎ 아직 반도 안왔어 ^&^",
+            1
+        ),
+        Friend(
+            profileImage = R.drawable.main,
+            name = "우상욱",
+            selfDescription = "나보다 안드 잘하는 사람 있으면 나와봐",
+            1
+        ),
+        Friend(
+            profileImage = R.drawable.main,
+            name = "배지현",
+            selfDescription = "표정 풀자 ^^",
+            1
+        ),
+        Friend(
+            profileImage = R.drawable.main,
+            name = "이의경",
+            selfDescription = "다들 빨리 끝내고 뒤풀이 가고 싶지? ㅎㅎ 아직 반도 안왔어 ^&^",
+            1
+        ),
+        Friend(
+            profileImage = R.drawable.main,
+            name = "우상욱",
+            selfDescription = "나보다 안드 잘하는 사람 있으면 나와봐",
+            1
+        ),
+        Friend(
+            profileImage = R.drawable.main,
+            name = "배지현",
+            selfDescription = "표정 풀자 ^^",
+            1
+        )
+    )
+}
--- app/src/main/java/com/sopt/now/UserViewHolder.kt
+++ app/src/main/java/com/sopt/now/UserViewHolder.kt
@@ -4,7 +4,8 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-class FriendViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+
+class UserViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
     val ivProfile : ImageView = view.findViewById(R.id.iv_profile)
     val tvName : TextView = view.findViewById(R.id.tv_name)
     val tvSelfDescription : TextView = view.findViewById(R.id.tv_self_description)
