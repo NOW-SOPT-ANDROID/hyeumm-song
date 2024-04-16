ea1f0047ab0f8309df68fbf066c8a6abcb381276
hyeumm <20221159@sungshin.ac.kr>
2024-04-17 00:59:59 +0900

    del/#6: 사용하지 않는 import 제거

--- app/src/main/java/com/sopt/now/compose/SignUpActivity.kt
+++ app/src/main/java/com/sopt/now/compose/SignUpActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -24,7 +23,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
