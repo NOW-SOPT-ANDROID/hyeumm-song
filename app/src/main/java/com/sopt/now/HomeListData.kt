2420f0ba3d68cdb332f33cd0e8847b6975f36491
hyeumm <20221159@sungshin.ac.kr>
2024-04-19 00:55:50 +0900

    chore/#5: 리스트 데이터 추가

--- app/src/main/java/com/sopt/now/HomeListData.kt
+++ app/src/main/java/com/sopt/now/HomeListData.kt
@@ -10,6 +10,24 @@ object HomeListData {
         ),
         HomeList(
             profileImage = R.drawable.main,
+            name = "박동민",
+            selfDescription = "곽의진...얼굴 재치 실력 모든걸 다 가진 남자... 하지만 밀양박씨 36대손인 나 박동민은 가지지 못했지",
+            1
+        ),
+        HomeList(
+            profileImage = R.drawable.main,
+            name = "이석준",
+            selfDescription = "죄송합니다 저 도핑했습니다... 안드-로이더 \uD83D\uDC89",
+            1
+        ),
+        HomeList(
+            profileImage = R.drawable.main,
+            name = "박유진",
+            selfDescription = "(ง˙∇˙)ว 에라 모르겠다",
+            1
+        ),
+        HomeList(
+            profileImage = R.drawable.main,
             name = "이의경",
             selfDescription = "다들 빨리 끝내고 뒤풀이 가고 싶지? ㅎㅎ 아직 반도 안왔어 ^&^",
             1
