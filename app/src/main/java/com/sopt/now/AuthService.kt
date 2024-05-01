1aca77ffb3a4cf55f69cc0b45b108cdc463655db
hyeumm <20221159@sungshin.ac.kr>
2024-05-02 00:10:40 +0900

    feat#10/: 비밀번호 변경 인터페이스

--- app/src/main/java/com/sopt/now/AuthService.kt
+++ app/src/main/java/com/sopt/now/AuthService.kt
@@ -2,11 +2,17 @@ package com.sopt.now
 
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 
 interface AuthService {
     @POST("member/join") //http 메소드
     fun signUp(
-        @Body request: RequestSignUpDto,
+        @Body request: RequestSignUpDto
     ): Call<ResponseSignUpDto> //비동기->callback
+
+    @PATCH("/member/password")
+    fun changePw(
+        @Body request : RequestChagePwDto
+    ) : Call<ResponseChangePwDto>
 }
