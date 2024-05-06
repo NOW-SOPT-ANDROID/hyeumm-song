e02b3c8f8766dda976f2505f08df56fecdf88a0d
hyeumm <20221159@sungshin.ac.kr>
2024-05-07 00:36:00 +0900

    feat#10/: 비밀번호 변경 버튼 추가

---
+++ app/src/main/java/com/sopt/now/presentation/main/mypage/ChangePasswordFragment.kt
@@ -0,0 +1,26 @@
+package com.sopt.now.presentation.main.mypage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sopt.now.R
+class ChangePasswordFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_change_password, container, false)
+    }
+}
