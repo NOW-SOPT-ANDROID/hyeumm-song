875b048e900655b1f3ea8e0d6b13c39beeee33f2
hyeumm <20221159@sungshin.ac.kr>
2024-05-07 01:11:27 +0900

    feat#10/: string 추출

---
+++ app/src/main/java/com/sopt/now/presentation/auth/changepassword/ChangePasswordActivity.kt
@@ -0,0 +1,21 @@
+package com.sopt.now.presentation.auth.changepassword
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.sopt.now.R
+
+class ChangePasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_change_password)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
