f0676e28e9658f88e8898057a06f431c2de23e56
hyeumm <20221159@sungshin.ac.kr>
2024-06-08 02:22:47 +0900

    mod#17 : getFollowerInfo 코루틴 적용

--- app/src/main/java/com/sopt/now/data/remote/service/FollwerService.kt
+++ app/src/main/java/com/sopt/now/data/remote/service/FollwerService.kt
@@ -1,13 +1,13 @@
 package com.sopt.now.data.remote.service
 
 import com.sopt.now.data.remote.dto.response.ResponseFollowerDto
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface FollwerService {
     @GET("/api/users")
-    fun getFollowerInfo(
+    suspend fun getFollowerInfo(
        @Query ("page") page : Int
-    ) : Call<ResponseFollowerDto>
+    ) : Response<ResponseFollowerDto>
 }
--- app/src/main/java/com/sopt/now/presentation/main/home/FollowerViewModel.kt
+++ app/src/main/java/com/sopt/now/presentation/main/home/FollowerViewModel.kt
@@ -3,11 +3,10 @@ package com.sopt.now.presentation.main.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.sopt.now.ServicePool
-import com.sopt.now.data.remote.dto.response.ResponseFollowerDto
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
 
 class FollowerViewModel : ViewModel() {
     private val followerService by lazy { ServicePool.followerService }
@@ -16,32 +15,18 @@ class FollowerViewModel : ViewModel() {
         get()=_liveData
 
     fun followerInfo(page:Int) {
-        followerService.getFollowerInfo(page).enqueue(object : Callback<ResponseFollowerDto> {
-            override fun onResponse(
-                call: Call<ResponseFollowerDto>,
-                response: Response<ResponseFollowerDto>,
-            ) {
-                if (response.isSuccessful) {
-                    val data: ResponseFollowerDto? = response.body()
-                    _liveData.value = FollowerState(
-                        isSuccess = true,
-                        message = "팔로워 정보 불러오기에 성공했습니다.",
-                    )
-                } else {
-                    val error = response.message()
-                    _liveData.value = FollowerState(
-                        isSuccess = false,
-                        message = "팔로워 정보 불러오기에 실패했습니다.  $error"
-                    )
-                }
-            }
-
-            override fun onFailure(call: Call<ResponseFollowerDto>, t: Throwable) {
+        viewModelScope.launch {
+            try {
+                _liveData.value = FollowerState(
+                    isSuccess = true,
+                    message = "팔로워 정보 불러오기에 성공했습니다.",
+                )
+            } catch (e: HttpException) {
                 _liveData.value = FollowerState(
                     isSuccess = false,
-                    message = "서버에러"
+                    message = "팔로워 정보 불러오기에 실패했습니다. ${e.code()}"
                 )
             }
-        })
+        }
     }
 }
