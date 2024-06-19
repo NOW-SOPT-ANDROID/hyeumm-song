7dd284e0f92893c93f06813b52d3dde9c0e2eb50
hyeumm <20221159@sungshin.ac.kr>
2024-06-20 00:45:46 +0900

    mod#18 : customTextField 보이기/숨김 여부 설정

--- app/src/main/java/com/sopt/now/compose/component/CustomTextfield.kt
+++ app/src/main/java/com/sopt/now/compose/component/CustomTextfield.kt
@@ -3,20 +3,14 @@ package com.sopt.now.compose.component
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
-import com.sopt.now.compose.R
-import com.sopt.now.compose.ui.theme.NOWSOPTAndroidTheme
 
 @Composable
 fun CustomTextField(
@@ -25,6 +19,7 @@ fun CustomTextField(
     @StringRes placeholderRes: Int,
     modifier: Modifier = Modifier,
     leadingIcon: @Composable (() -> Unit)? = null,
+    isPassword : Boolean = false
 ) {
     TextField(
         value = value,
@@ -34,7 +29,7 @@ fun CustomTextField(
             .padding(10.dp),
         placeholder = { Text(stringResource(placeholderRes)) },
         singleLine = true,
-        visualTransformation = PasswordVisualTransformation(),
-        leadingIcon = leadingIcon
+        leadingIcon = leadingIcon,
+        visualTransformation = if (isPassword) PasswordVisualTransformation() else VisualTransformation.None
     )
 }
