9efe4beb934e6cbbee01ae01267c5bc978c24477
hyeumm <20221159@sungshin.ac.kr>
2024-06-22 04:28:04 +0900

    feat#18 : sideEffect 적용

---
+++ app/src/main/java/com/sopt/now/compose/HomeSideEffect.kt
@@ -0,0 +1,6 @@
+package com.sopt.now.compose
+
+sealed class HomeSideEffect {
+    //data class SnackBar(val message: Int) : HomeSideEffect()
+    data class SnackBar(val message: String) : HomeSideEffect() //잠깐 문자열 하드코딩하느라 바꿈
+}
