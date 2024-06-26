a3200f4921caf79b880cf369b2d879d0a3c03806
hyeumm <20221159@sungshin.ac.kr>
2024-06-27 00:03:53 +0900

    del#18 : SoptApplication.kt 구현 코드 삭제

--- app/src/main/java/com/sopt/now/compose/SoptApplication.kt
+++ app/src/main/java/com/sopt/now/compose/SoptApplication.kt
@@ -4,8 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class SoptApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
+class SoptApplication : Application()
